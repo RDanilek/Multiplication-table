3b5e583437af4d57d6d01dcfee59a62a36826156
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-09-21 14:37:06 -0400

    Updated from Brython Server: 9/21/2015 2:37:05 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -21,8 +21,14 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+print("The number is: {0:>3}".format(1))         # Prints "The number is:   1"
+print("The number is: {0:>3}".format(12))        # Prints "The number is:  12"
+print("The number is: {0:>3}".format(123)) 
+
 a = int(input("Width of multiplication table: "))
 b = int(input("Height of multiplication table: "))
 for l in range(1,a):
     for i in range(1,b):
         print(i*l, end='  ')
+        
+        
